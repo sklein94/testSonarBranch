47440b564bd973869c286d2082253a438558504b
Simon Klein <simon.klein@cloudogu.com>
2020-01-31 15:01:22 +0100

    test

--- main.go
+++ main.go
@@ -7,4 +7,27 @@ import (
 func main() {
 	fmt.Println("main")
  	fmt.Println("main2")
+ 	fmt.Println("main2")
+ 	fmt.Println("main2")
+ 	fmt.Println("main2")
+ 	fmt.Println("main2")
+ 	fmt.Println("main2")
+ 	fmt.Println("main2")
+ 	fmt.Println("main2")
+ 	fmt.Println("main2")
+ 	fmt.Println("main2")
+}
+
+
+
+func test(){
+   fmt.Println("test")
+}
+
+func test3(){
+   fmt.Println("test")
+}
+
+func test3(){
+   fmt.Println("test")
 }
